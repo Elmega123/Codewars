34b5fc70aacce033731a643c89d63a60128495e8
Elmega123 <yourpalbussiness@gmail.com>
2022-09-09 11:18:54 -0500

    another commit

---
+++ DNA_to_RNA_Conversion.js
@@ -0,0 +1,11 @@
+function DNAtoRNA(dna) {
+    // create a function which returns an RNA sequence from the given DNA sequence
+
+    let heh = dna.replace(/T/g,"U")
+
+    console.log(heh)
+
+  }
+
+
+  DNAtoRNA("GCTT")

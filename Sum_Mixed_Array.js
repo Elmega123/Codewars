b796b6dcfc889105011ee8422250a37c74389b56
Elmega123 <yourpalbussiness@gmail.com>
2022-09-09 12:13:59 -0500

    another commit

---
+++ Sum_Mixed_Array.js
@@ -0,0 +1,18 @@
+function sumMix(x){
+    let summation = 0
+
+    for (let index = 0; index < x.length; index++) {
+        
+        summation  += Number(x[index])
+
+        
+    }
+
+
+
+   return summation
+
+}
+
+
+sumMix([9, 3, '7', '3'])

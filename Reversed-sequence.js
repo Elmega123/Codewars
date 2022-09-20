ed9cfa158624c96ba14649e71026a5e5722ad09a
Elmega123 <yourpalbussiness@gmail.com>
2022-09-20 12:15:59 -0500

    another commit

---
+++ Reversed-sequence.js
@@ -0,0 +1,21 @@
+
+
+
+const reverseSeq = n => {
+
+    let arr = []
+
+
+    for (let index = n; index > 0; index--) {
+        
+        console.log(index)
+
+        arr.push(index)
+    }
+
+   return arr
+    
+  };
+
+
+  reverseSeq(5)

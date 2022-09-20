65c9170598dfc57d30ec4f8bc1d69c7af235a83f
Elmega123 <yourpalbussiness@gmail.com>
2022-09-20 18:32:14 -0500

    another commit

---
+++ Is_he_gonna_survive.js
@@ -0,0 +1,17 @@
+function hero(bullets, dragons){
+    //Get Coding!
+
+    if(dragons*2 > bullets){
+
+        console.log('he is dead')
+       return false
+       
+    }else{
+        console.log('he lives')
+        return true
+        
+    }
+    }
+
+    
+hero(9,5)

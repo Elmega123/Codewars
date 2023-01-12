fd7422117657125b0c6cd37a185369f28d3db9bd
Pablo De La O <yourpalbussiness@gmail.com>
2023-01-11 18:04:18 -0600

    another commit

---
+++ Count-monkeys.js
@@ -0,0 +1,13 @@
+function monkeyCount(n) {
+    // your code here
+      let arr = []    
+
+
+      for (let index = 1; index <=n; index++) {
+        arr.push(index)
+      }
+      return arr
+    }
+
+
+    console.log(monkeyCount(6))
